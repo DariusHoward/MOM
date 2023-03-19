226a91353c77e15abfdd330c70df69f984dd00e9
El Dario Howard <31dariushoward@gmail.com>
2023-03-19 15:08:57 -0500

    Add files via upload
    Code for MOM program.

---
+++ MOM.py
@@ -0,0 +1,16 @@
+"This is for a mother on Mother's Day"
+
+#User has to select what my favorite food is 
+
+Yes = "Yes"
+
+Question = input("Are you a mom?" + "\n")
+
+if Question == Yes:
+    print("Happy Mother's Day!")
+    print("Thanks for being a MOM!")
+
+else:
+    print("Ooops!")
+    print("Not a MOM!")
+
